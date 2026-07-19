7ca8c194feb9515a4302249814fb59114195d98d
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-07-19 10:57:01 -0600

    Wait for reload until after services are up

--- sigic_install.sh
+++ sigic_install.sh
@@ -278,7 +278,6 @@ NGINXEOF
   # Agregar nuevo host al mapping si no existe
   if ! $(grep -Fq "${HOSTNAME} nginx4${COMPOSE_PROJECT_NAME}" "$PROXY_STREAM_DEFAULT"); then
     sed -i "s/backend {/backend {\n    ${HOSTNAME} nginx4${COMPOSE_PROJECT_NAME}:443;/" "$PROXY_STREAM_DEFAULT"
-    docker exec nginx-proxy nginx -s reload
   fi
 
   # En fresh install las imágenes de frontend no existen localmente — construirlas antes del up
