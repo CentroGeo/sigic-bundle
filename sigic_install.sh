29ae54e63d5a6b3654761f9744d2167f9f54011d
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-07-20 09:57:35 -0600

    Add acme-challenge passing to inner nginx

--- sigic_install.sh
+++ sigic_install.sh
@@ -265,6 +265,19 @@ server {
 
     large_client_header_buffers 4 16k;
 
+    location ^~ /.well-known/acme-challenge/ {
+        proxy_pass http://nginx4${COMPOSE_PROJECT_NAME}/.well-known/acme-challenge/;
+
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+
+        proxy_buffer_size          128k;
+        proxy_buffers              4 256k;
+        proxy_busy_buffers_size    256k;
+    }
+
     location / {
         proxy_pass https://nginx4${COMPOSE_PROJECT_NAME};
 
