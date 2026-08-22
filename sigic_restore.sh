3eaae2511d283c63c6ac2b2a6b317fe1552e6b75
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-08-21 18:09:44 -0600

    Fix restore getting SOURCE_URL

--- sigic_restore.sh
+++ sigic_restore.sh
@@ -8,7 +8,7 @@ BACKEND_CONTAINER="django4$COMPOSE_PROJECT"
 FRONTEND_CONTAINER="frontendadmin4$COMPOSE_PROJECT"
 BACKUP_PATH="backup_$COMPOSE_PROJECT/"
 
-SOURCE_URL=$(cat "$BACKUP_PATH/source_url")
+SOURCE_URL=$(cat "$BACKUP_PATH/source_url" | grep -E '^(STATIC_URL)=')
 TARGET_URL=$(cat "$ENVFILE" | grep -E '^(STATIC_URL)=')
 
 echo "Restaurando Keycloak realm e usuarios"
