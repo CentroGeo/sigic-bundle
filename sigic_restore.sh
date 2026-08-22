cc42ad0a82d8cf0de32acae2cb1cfe6ce4184a29
Héctor de la Rosa Prado <hoftherose@gmail.com>
2026-08-22 00:03:25 -0600

    Fixes in renaming keycloak files

--- sigic_restore.sh
+++ sigic_restore.sh
@@ -8,13 +8,14 @@ KEYCLOAK_CONTAINER="keycloak4$COMPOSE_PROJECT"
 POSTGRES_CONTAINER="db4$COMPOSE_PROJECT"
 BACKEND_CONTAINER="django4$COMPOSE_PROJECT"
 FRONTEND_CONTAINER="frontendadmin4$COMPOSE_PROJECT"
-BACKUP_PATH="backup_$COMPOSE_PROJECT/"
+BACKUP_PATH="backup_$COMPOSE_PROJECT"
 
 if [ "$PREV_COMPOSE_PROJECT" != "$COMPOSE_PROJECT" ]; then
     echo "Se requiere migrar el respaldo al nuevo proyecto"
     echo "$PREV_COMPOSE_PROJECT -> $COMPOSE_PROJECT"
     cp -r "backup_$PREV_COMPOSE_PROJECT/" $BACKUP_PATH
-    cp "$BACKUP_PATH/$PREV_COMPOSE_PROJECT-landing_builder_data.tar.gz" "$BACKUP_PATH/$COMPOSE_PROJECT-landing_builder_data.tar.gz"
+    mv "$BACKUP_PATH/$PREV_COMPOSE_PROJECT-landing_builder_data.tar.gz" "$BACKUP_PATH/$COMPOSE_PROJECT-landing_builder_data.tar.gz"
+    for config in $BACKUP_PATH/keycloak/export/$PREV_COMPOSE_PROJECT*; do mv "$config" $(echo "$config" | sed "s/$COMPOSE_PROJECT/$PREV_COMPOSE_PROJECT/g"); done
     find $BACKUP_PATH -type f -exec sed -i "s|$PREV_COMPOSE_PROJECT|$COMPOSE_PROJECT|g" {} +
     echo "Archivos migrados"
 fi
